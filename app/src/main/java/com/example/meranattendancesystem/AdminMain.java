7c1193ed04b87966002e44656fd5d47e996a8733
saraibrahim99 <saraibrahim654@gmail.com>
2019-07-10 20:51:36 +0300

    Sara's Update

---
+++ app/src/main/java/com/example/meranattendancesystem/AdminMain.java
@@ -0,0 +1,63 @@
+package com.example.meranattendancesystem;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AdminMain extends Fragment {
+
+    private BottomNavigationView nav;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.admin_main, null);
+        //Go to ScanPage
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.m_container, new ScanQR())
+                .commit();
+
+        //NavigationBar
+        nav = (BottomNavigationView) v.findViewById(R.id.a_m_nav);
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.n_scan:
+                        getActivity().getSupportFragmentManager()
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.m_container, new ScanQR())
+                                .commit();
+                        break;
+                    case R.id.n_charts:
+                        /*getActivity().getSupportFragmentManager()
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.m_container, new Chart())
+                                .commit();*/
+                        break;
+                    case R.id.n_generate:
+                        /*getActivity().getSupportFragmentManager()
+                                .beginTransaction()
+                                .addToBackStack(null)
+                                .replace(R.id.m_container, new GenerateQR())
+                                .commit();*/
+                        break;
+                }
+                return true;
+            }
+        });
+        return v;
+    }
+}
